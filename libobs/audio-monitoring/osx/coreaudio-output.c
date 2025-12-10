f48b223f4d1c767fc1eb0773af3cbd4512623abc
pkv <pkv@obsproject.com>
2025-12-10 01:40:11 +0100

    libobs/audio_monitoring: Allow monitoring of muted sources
    This allows the monitoring of muted sources.
    In previous monitoring_type API, it was possible to monitor a visible
    source without outputting its audio.
    With the new API, visibility is now tied directly to what is output.
    There are therefore two options now:
    - either hide the source and monitor it (by displaying inactive sources
    in the mixer);
    - or mute the source, show it and monitor it.
    The first option is already possible; we allow now the second option.
    The principle used is that we should be able to monitor any vue-meter
    which shows activity.
    Since a muted source as well as a hidden one may show audio activity,
    we must allow their monitoring.
    
    Signed-off-by: pkv <pkv@obsproject.com>

--- libobs/audio-monitoring/osx/coreaudio-output.c
+++ libobs/audio-monitoring/osx/coreaudio-output.c
@@ -63,6 +63,7 @@ static void on_audio_pause(void *data, calldata_t *calldata)
 
 static void on_audio_playback(void *param, obs_source_t *source, const struct audio_data *audio_data, bool muted)
 {
+	UNUSED_PARAMETER(muted);
 	struct audio_monitor *monitor = param;
 	float vol = source->user_volume;
 	uint32_t bytes;
@@ -88,17 +89,13 @@ static void on_audio_playback(void *param, obs_source_t *source, const struct au
 
 	bytes = sizeof(float) * monitor->channels * resample_frames;
 
-	if (muted) {
-		memset(resample_data[0], 0, bytes);
-	} else {
-		/* apply volume */
-		if (!close_float(vol, 1.0f, EPSILON)) {
-			register float *cur = (float *)resample_data[0];
-			register float *end = cur + resample_frames * monitor->channels;
+	/* apply volume */
+	if (!close_float(vol, 1.0f, EPSILON)) {
+		register float *cur = (float *)resample_data[0];
+		register float *end = cur + resample_frames * monitor->channels;
 
-			while (cur < end)
-				*(cur++) *= vol;
-		}
+		while (cur < end)
+			*(cur++) *= vol;
 	}
 
 	pthread_mutex_lock(&monitor->mutex);
